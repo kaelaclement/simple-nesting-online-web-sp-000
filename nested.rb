7b0a4fe0aab459bb2141eecbc4b73a05b366314e
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-27 04:09:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -84,7 +82,7 @@ def adding_matz
         }
      }
 
-
+	programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 end
 
 def changing_alan
